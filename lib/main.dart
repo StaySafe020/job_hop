917b406e47bb2bd517c4434c52225397d2fc4562
jambong-ralpher <jambongralpher@gmail.com>
2025-01-30 13:05:38 +0100

    project structure update

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:job_app/notifications/notification_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
-import '../services/connection_service.dart'; // Import your connection service
-import 'onboard screen/onboard.dart';
-import 'onboard screen/pageview.dart';
-import 'authentication/login_screen.dart';
-import 'authentication/registration_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
-  await Firebase.initializeApp(); // Initialize Firebase
-
-  // Test Firebase connection
-  ConnectionService connectionService = ConnectionService();
-  await connectionService.checkConnection(); // Call the check method
-  
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => NotificationProvider(),
-      child: MyApp(),
-    ),
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Job Hop',
-      debugShowCheckedModeBanner: false,
+      title: 'JobConnect',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        useMaterial3: true,
       ),
-      initialRoute: '/onboarding',
-      routes: {
-        '/onboarding': (context) => const OnboardScreen(),
-        '/pageview': (context) => const OnboardingPageView(),
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegistrationScreen(),
-      },
+      home: LoginScreen(), // Start with the login screen
     );
   }
 }
